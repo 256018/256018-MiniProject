b7db8c3495960cc2a14dfad75a5ad6e652ee0323
Rohan Gupta <47993921+256018@users.noreply.github.com>
2021-04-23 22:12:07 +0530

    Update MainMenu.py

--- src/MainMenu.py
+++ src/MainMenu.py
@@ -1,1 +1,25 @@
+def mainmenu():
+    print('*********WELCOME TO ATM**********\n')
+    print("Enter 1 to Login")
+    print("Enter 2 to create an Account")
+    print("Enter 3 to exit the menu")
+    choice = int(input())
+    if choice == 1:
+        clear_screen()
+        try:
+            login(accounts_list)
+        except KeyboardInterrupt:
+            clear_screen()
+    elif choice == 2:
+        create_account(accounts_list)
+    elif choice == 3:
+        # close the program
+        exit(0)
+    else:
+        clear_screen()
+        print("ERROR: Wrong choice\n")
+    mainmenu()
 
+
+if __name__ == '__main__':
+    mainmenu()
